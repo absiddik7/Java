913b7d67a646833fa9f76de3e75e09a3f53893a4
abSiddik <abubakkar1070@gmail.com>
2021-01-20 00:09:17 +0600

    varriable second commit

--- Varriable.java
+++ Varriable.java
@@ -2,5 +2,9 @@ public class Varriable {
     public static void main(String[] args) {
         
         System.out.println("Learn Java!");
+
+        System.out.println("Learn Java!");
+        System.out.println("Learn Java!");
+        
     }
 }
