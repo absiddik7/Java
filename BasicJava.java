9505975a9b09479709e38874985bff2fecc82109
abSiddik <abubakkar1070@gmail.com>
2021-01-20 23:36:38 +0600

    data types and input

---
+++ BasicJava.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class BasicJava{
+    public static void main(String[] args){
+        
+
+        // data types
+        int speedLimit = 80;
+        float fraction = 2.5f;
+        double doubleNum = 2.5;
+        char c = 'f';
+        char letter = '\u0051'; // it is Q
+        String str = "Hello programmer";
+        boolean flag1 = true;
+        long total = -40030303298L;
+        byte range = 124; // byte data types. range -128 to 127, 8 bit integer.
+        short temperature = -200; // short data types. range -32768 to 32767, 16 bit integer.
+
+        System.out.println("fraction = " + fraction);
+        System.out.println(doubleNum);
+        System.out.println(str);
+        System.out.println(flag1);
+        System.out.println(letter); // prints Q
+
+        // Java Ternary Operator
+        int februaryDays = 29;
+        String result;
+        result = (februaryDays == 28) ? "Not a leap year" : "leap year";
+        System.out.println(result);
+
+
+        // user input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = input.nextInt();
+        System.out.println("You entered = " + number);
+        input.close();
+
+
+    }
+}
--- Loop.java
+++ Loop.java
@@ -6,7 +6,7 @@ public class Loop {
         System.out.println("Learn  for Loop!");
 
         System.out.println("Learn while Loop!");
-        
+
         System.out.println("Learn Loop!");
     }  
 }
